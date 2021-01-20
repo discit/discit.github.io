ff653bd1d736ac288824e53b0985f78ab3917f09
sourceoflife <nhuuviet@yahoo.com>
2021-01-20 15:32:40 -0600

    .

--- w2d1/assignment1/textdecorate.js
+++ w2d1/assignment1/textdecorate.js
@@ -1,44 +1,40 @@
+(function () {
+    "use strict";
+    window.onload = function init() {
+        let btn = document.getElementById("bigbnt").onclick = increaseFont;
+        let checkE = document.getElementById("billingID").onchange = changeFontWeigh;
+    }
 
-let btn = document.getElementById("bigbnt");
-let checkE = document.getElementById("billingID");
-
-btn.onclick = increaseFont;
-
-checkE.onchange = changeFontWeigh;
-
-function increaseFont()
-{
+    function increaseFont() {
         setInterval(changeFont, 500);
-}
-function changeFont()
-{
-    // alert("Hello, World");
-    let textE = document.getElementById("mytextarea");
-    // alert(parseInt(textE.style.font.fontsize)); 
-   if(textE.style.fontSize =="") {
-        textE.style.fontSize = "12pt";
-   }
-    else {
-        textE.style.fontSize = parseInt(textE.style.fontSize) + 2 + "px";
     }
+    function changeFont() {
+        // alert("Hello, World");
+        let textE = document.getElementById("mytextarea");
+        // alert(parseInt(textE.style.font.fontsize)); 
+        if (textE.style.fontSize == "") {
+            textE.style.fontSize = "12pt";
+        }
+        else {
+            textE.style.fontSize = parseInt(textE.style.fontSize) + 2 + "px";
+        }
 
-}
-function changeFontWeigh()
-{
-    // alert("hello");
-    let checkE = document.getElementById("billingID");
-    // alert(checkE.checked);
+    }
+    function changeFontWeigh() {
+        // alert("hello");
+        let checkE = document.getElementById("billingID");
+        // alert(checkE.checked);
 
-    let textE = document.getElementById("mytextarea");
-    if(checkE.checked == true) 
-    {
-        textE.style.fontWeight = "bold";
-        textE.style.color = "green";
-        textE.style.textDecoration = "underline";
+        let textE = document.getElementById("mytextarea");
+        if (checkE.checked == true) {
+            textE.style.fontWeight = "bold";
+            textE.style.color = "green";
+            textE.style.textDecoration = "underline";
+        }
+        else {
+            textE.style.fontWeight = "";
+            textE.style.color = "";
+            textE.style.textDecoration = "";
+        }
     }
-    else {
-        textE.style.fontWeight = "";
-        textE.style.color = "";
-        textE.style.textDecoration = "";
-    }
-}       
+})();
