d74f52efc503668683d7b76cd98bb2ac0c4a284f
sourceoflife <nhuuviet@yahoo.com>
2021-01-20 15:50:52 -0600

    .

--- w2d1/assignment2/ascci.js
+++ w2d1/assignment2/ascci.js
@@ -1,159 +1,62 @@
-let btnStart = document.getElementById("start");
+(function () {
+    "use strict";
 
-btnStart.onclick = start;
+    window.onload = function () {
+        document.getElementById("start").onclick = start;
 
-let btnStop = document.getElementById("stop");
+        document.getElementById("stop").onclick = stop;
 
-btnStop.onclick = stop;
-
-let intervalID;
-let intervalNumber = 250;
-
-
-let animationE = document.getElementById("animation");
-
-animationE.onchange = aniChange;
-
-function aniChange() {
-    // alert("animation");
-
-    let textE = document.getElementById("mytextarea");
-
-    switch (animationE.value) {
-        case "Exercise":
-            textE.value = EXERCISE;
-            break;
-        case "Juggler":
-            textE.value = JUGGLER;
-            break;
-        case "Bike": ;
-            textE.value = BIKE;
-            break;
-        case "Dive":
-            textE.value = DIVE
-            break;
-        case "Blank":
-            textE.value = "";
-            break;
-
-        default:
-            break;
+        document.getElementById("size").onchange = sizeChange;
+        document.getElementById("animation").onchange = aniChange;
     }
 
-}
+    let intervalID;
+    let intervalNumber = 250;
 
+    function aniChange() {
+        // alert("animation");
 
-let sizeE = document.getElementById("size");
+        let textE = document.getElementById("mytextarea");
 
-sizeE.onchange = sizeChange;
+        switch (document.getElementById("animation").value) {
+            case "Exercise":
+                textE.value = EXERCISE;
+                break;
+            case "Juggler":
+                textE.value = JUGGLER;
+                break;
+            case "Bike": ;
+                textE.value = BIKE;
+                break;
+            case "Dive":
+                textE.value = DIVE
+                break;
+            case "Blank":
+                textE.value = "";
+                break;
 
-function sizeChange() {
-    let textE = document.getElementById("mytextarea");
-    // alert(parseInt(textE.style.font.fontsize)); 
+            default:
+                break;
+        }
 
-    textE.style.fontSize = document.getElementById("size").value;
-
-}
-function start() {
-    // alert(animationE.value);
-    // let textE = document.getElementById("textareaID");
-    // let textE = document.getElementById("mytextarea");
-    // alert(animationE.value);
-
-
-    document.getElementById("start").disabled = true;
-    document.getElementById("animation").disabled = true;
-    document.getElementById("stop").disabled = false;
-
-    let textE = document.getElementById("mytextarea");
-
-    switch (animationE.value) {
-        case "Exercise":
-            display(EXERCISE);
-            break;
-        case "Juggler":
-            display(JUGGLER);
-            break;
-        case "Bike": ;
-            display(BIKE);
-            break;
-        case "Dive":
-            display(DIVE);
-            break;
-        case "Blank":
-            textE.value = "";
-            break;
-
-        default:
-            break;
     }
 
 
-}
-
-function display(arr) {
-    let frames = arr.split("=====");
-    let textE = document.getElementById("mytextarea");
-    let n = frames.length;
-    let i = 0;
-
-    // alert(frames[0]);
-    intervalID = setInterval(() => {
-        if (i >= n)
-            i = 0;
-
-        textE.value = frames[i];
-        i++;
-    }, intervalNumber);
-}
-
-function stop() {
-    clearInterval(intervalID);
-    let textE = document.getElementById("mytextarea");
-    document.getElementById("stop").disabled = true;
 
 
-    document.getElementById("start").disabled = false;
-    document.getElementById("animation").disabled = false;
+    function sizeChange() {
+        let textE = document.getElementById("mytextarea");
+        textE.style.fontSize = document.getElementById("size").value;
 
-
-    switch (animationE.value) {
-        case "Exercise":
-            textE.value = EXERCISE;
-            break;
-        case "Juggler":
-            textE.value = JUGGLER;
-            break;
-        case "Bike": ;
-            textE.value = BIKE;
-            break;
-        case "Dive":
-            textE.value = DIVE
-            break;
-        case "Blank":
-            textE.value = "";
-            break;
-
-        default:
-            break;
     }
-}
+    function start() {
+        document.getElementById("start").disabled = true;
+        document.getElementById("animation").disabled = true;
+        document.getElementById("stop").disabled = false;
 
-let speedE = document.getElementById("speed");
+        let textE = document.getElementById("mytextarea");
 
-speedE.onchange = speedChange;
-
-function speedChange() {
-    let checkE = document.getElementById("speed");
-    // alert(checkE.checked);
-    if (checkE.checked == true)
-        intervalNumber = 50;
-    else {
-        intervalNumber = 250;
-    }
-    if (document.getElementById("start").disabled) {
-        clearInterval(intervalID);
-        switch (animationE.value) {
+        switch (document.getElementById("animation").value) {
             case "Exercise":
                 display(EXERCISE);
                 break;
@@ -173,5 +76,92 @@ function speedChange() {
             default:
                 break;
         }
+
+
     }
-}
+
+    function display(arr) {
+        let frames = arr.split("=====");
+        let textE = document.getElementById("mytextarea");
+        let n = frames.length;
+        let i = 0;
+
+        // alert(frames[0]);
+        intervalID = setInterval(() => {
+            if (i >= n)
+                i = 0;
+
+            textE.value = frames[i];
+            i++;
+        }, intervalNumber);
+    }
+
+    function stop() {
+        clearInterval(intervalID);
+        let textE = document.getElementById("mytextarea");
+        document.getElementById("stop").disabled = true;
+
+
+        document.getElementById("start").disabled = false;
+        document.getElementById("animation").disabled = false;
+
+
+        switch (document.getElementById("animation").value) {
+            case "Exercise":
+                textE.value = EXERCISE;
+                break;
+            case "Juggler":
+                textE.value = JUGGLER;
+                break;
+            case "Bike": ;
+                textE.value = BIKE;
+                break;
+            case "Dive":
+                textE.value = DIVE
+                break;
+            case "Blank":
+                textE.value = "";
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    let speedE = document.getElementById("speed");
+
+    speedE.onchange = speedChange;
+
+    function speedChange() {
+        let checkE = document.getElementById("speed");
+        // alert(checkE.checked);
+        if (checkE.checked == true)
+            intervalNumber = 50;
+        else {
+            intervalNumber = 250;
+        }
+        if (document.getElementById("start").disabled) {
+            clearInterval(intervalID);
+            switch (document.getElementById("animation").value) {
+                case "Exercise":
+                    display(EXERCISE);
+                    break;
+                case "Juggler":
+                    display(JUGGLER);
+                    break;
+                case "Bike": ;
+                    display(BIKE);
+                    break;
+                case "Dive":
+                    display(DIVE);
+                    break;
+                case "Blank":
+                    textE.value = "";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+})();
