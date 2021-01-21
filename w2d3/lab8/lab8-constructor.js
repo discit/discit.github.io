054e06866536328c92b4b23d02cab53451c52e0a
sourceoflife <nhuuviet@yahoo.com>
2021-01-21 08:59:51 -0600

    .

--- w2d3/lab8/lab8-constructor.js
+++ w2d3/lab8/lab8-constructor.js
@@ -1,38 +1,29 @@
 (function () {
     "use strict";
     function MountainBike(gearV) {
-
         this.gear = gearV;
         this.setGear = function (gear) {
             this.gear = gear;
         }
-
     }
-
     MountainBike.prototype.speed = 0;
-
     MountainBike.prototype.applyBrake = function (num) { this.speed -= num };
     MountainBike.prototype.speedup = function (num) { this.speed += num };
-
   
-    var a = new MountainBike(1);
+       function start() {
+        var mountainBike = new MountainBike(1);
 
- 
-    function start() {
-        var mountainBikePrototype = new MountainBike(1);
+        console.log("mountainBike before set speed");
+        console.log(mountainBike);
+        mountainBike.speedup(20);
+        console.log("mountainBike after set speed");
+        console.log(mountainBike);
 
-        console.log("mountainBikePrototype before set speed");
-        console.log(mountainBikePrototype);
-        mountainBikePrototype.speedup(20);
-        console.log("mountainBikePrototype after set speed");
-        console.log(mountainBikePrototype);
-
-        console.log("mountainBikePrototype before set setGear");
-        console.log(mountainBikePrototype);
-        mountainBikePrototype.setGear(2);
-        console.log("mountainBikePrototype after set setGear");
-        console.log(mountainBikePrototype);
-
+        console.log("mountainBike before set setGear");
+        console.log(mountainBike);
+        mountainBike.setGear(2);
+        console.log("mountainBike after set setGear");
+        console.log(mountainBike);
     }
     start();
 
