7ce391b67ff48ccbdbae0e364e69648d20ee541b
sourceoflife <nhuuviet@yahoo.com>
2021-01-26 15:38:54 -0600

    .

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,27 +1,43 @@
 
 window.onload = function () {
-    document.getElementById("btn").onclick = getUser2;
+    document.getElementById("btn").onclick = getPosts;
+   // document.getElementById("userid").onchange = enableSubmit;
+
 }
+function enableSubmit()
+{
+   // document.getElementById("btn").disabled = false;
+}
+function getComments() {
+    this.disabled = true;
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${this.dataset.postid}`)
+        .then(res => res.json())
+        .then(displayComments)
+        .catch(console.error);
+    }
 
 
-// async function getUser() {
-//     // alert("hello");
-//     let response = await fetch("http://jsonplaceholder.typicode.com/users/1");
-//     if (response.ok) { // if HTTP-status is 200-299
-//         // get the response body (the method explained below)
-//         let json = await response.json();
-//         document.getElementById("userinfo").value = json.company.name;
-//     } else {
-//         alert("HTTP-Error: " + response.status);
-//     }
-// };
+function displayComments(comments) {
+    //console.log(comments);
+    if (comments.length == 0) return ;
 
+ 
+    let commentE = document.getElementById(`c_${comments[0].postId}`);
+    comments.forEach(comment => {
+        commentE.insertAdjacentHTML('beforeend', `<div class="comment">
+              <h4>${comment.name}</h4>
+              <p> ${comment.email}</p>
+              <p>${comment.body}</p>
+            </div>
+          `)
+    });
+  }
 
-
-function getUser2() {
+function getPosts() {
     // alert("hello");
-    let user = "http://jsonplaceholder.typicode.com/users/" + document.querySelector('#userid').value;
-    let userPost = "http://jsonplaceholder.typicode.com/posts?users/" + document.querySelector('#userid').value;
+    this.disabled = true;
+    let user = "https://jsonplaceholder.typicode.com/users/" + document.querySelector('#userid').value;
+    let userPost = "https://jsonplaceholder.typicode.com/posts?users/" + document.querySelector('#userid').value;
 
     fetch(user)
         .then(res => res.text())
@@ -30,8 +46,6 @@ function getUser2() {
             let username = json.name;
             let email = json.email;
             let addrress = json.address;
-
-            
             let div = document.createElement('div');
             div.innerHTML = `
             <p><strong>${username}</strong></p>
@@ -41,11 +55,10 @@ function getUser2() {
             <p>\t street: ${addrress.suite}</p>
             <p>\t street: ${addrress.city}</p>
             <p>\t street: ${addrress.zip}</p>
-    
-            `;
+                `;
             document.querySelector('#container').prepend(div);
-
         })
+        .catch(console.error);
 
     fetch(userPost)
         .then(res => res.text())
@@ -54,27 +67,19 @@ function getUser2() {
             let userdiv = document.querySelector('#container');
             for (let e of json) {
                 console.log(e.id + " " + e.title);
-
                 let div = document.createElement('div');
-                // 2. Set its class to "alert"
-                // div.className = "alert";
                 div.id = "post" + e.id;
-                // 3. Fill it with the content
                 div.innerHTML = `<p><strong>${e.title}</strong></p>
                 <p>${e.body}</p>
-                <button id="bnt${e.id}">comment</button>
+                <button id="bnt${e.id}" data-postid="${e.id}">Load comment</button>
+                <div class="comment" id="c_${e.id}"></div>
+                
                 `;
                 userdiv.append(div);
-
+                document.querySelector(`#bnt${e.id}`).onclick = getComments;
             }
-            // document.querySelector('#username').innerHTML = username;
-
-            // {
-            //     "userId": 1,
-            //     "id": 1,
-            //     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            //     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-            //   }, 
         })
+        .catch(console.error);
+        
 }
 
